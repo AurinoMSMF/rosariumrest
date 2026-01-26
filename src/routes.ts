e1a606f901d85c1bca644d71544249e3fcd069cc
Aurino <aurinodoc@gmail.com>
2026-01-26 01:14:50 -0300

    Update: Trocando de commonjs para esmodules e configurando arquivo de deploy da vercel.

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,14 @@
 import { Router } from "express";
 import { getDayMysteryController } from "./use-cases/GetDayMystery";
 
-const router = Router(); 
+const router = Router();
 
 router.get("/day-mystery/", (request, response) => {
-    return getDayMysteryController.handle(request, response);
+  return getDayMysteryController.handle(request, response);
 });
 
 router.get("/day-mystery/:day", (request, response) => {
-    return getDayMysteryController.handle(request, response);
+  return getDayMysteryController.handle(request, response);
 });
 
 export { router };
-
