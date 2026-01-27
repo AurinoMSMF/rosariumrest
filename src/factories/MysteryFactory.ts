7cc82a59731f5e38ec75baafaecd674f289917f5
Aurino <aurinodoc@gmail.com>
2026-01-27 20:11:46 -0300

    Feature: Adicionado 'type' (tipo) ao retorno da requisição dos mistérios do dia. Os tipos possíveis são: mysteriaGloriosa, mysteriaDolorosa, mysteriaGaudiosa.

--- src/factories/MysteryFactory.ts
+++ src/factories/MysteryFactory.ts
@@ -2,24 +2,31 @@ import { mysteriesJson } from "../sanctum-rosarium.json.js";
 
 export class MysteryFactory {
   // [0,1,2,3,4,5,6]
-  static #gloriosa = [0, 3];
-  static #dolorosa = [2, 5];
-  static #gaudiosa = [1, 4, 6];
+  static #gloriosa = [1, 4];
+  static #dolorosa = [3, 6];
+  static #gaudiosa = [2, 5, 7];
   static #mysteriesJson = mysteriesJson;
 
   static buildMystery(weekdayNumber: number) {
-    let dayMystery: string;
+    let dayMysteriesType: string;
 
     if (this.#gloriosa.includes(weekdayNumber)) {
-      dayMystery = "mysteriaGloriosa";
+      dayMysteriesType = "mysteriaGloriosa";
     } else if (this.#dolorosa.includes(weekdayNumber)) {
-      dayMystery = "mysteriaDolorosa";
+      dayMysteriesType = "mysteriaDolorosa";
     } else if (this.#gaudiosa.includes(weekdayNumber)) {
-      dayMystery = "mysteriaGaudiosa";
+      dayMysteriesType = "mysteriaGaudiosa";
     } else {
       throw new Error("Invalid week day.");
     }
 
-    return this.#mysteriesJson[dayMystery];
+    const dayMysteries = this.#mysteriesJson[dayMysteriesType];
+
+    const completeDayMystery = {
+      type: dayMysteriesType,
+      mysteries: dayMysteries,
+    };
+
+    return completeDayMystery;
   }
 }
